5a53ef27882166f1804a5c05d24f822e5ebf56fe
Hylandras <hylandras@tutanota.com>
2015-11-02 19:47:52 +0100

    Prompt user to ask if OCR is needed
    We use the shell script to test if OCR has to be done and if not, we
    only create the final preview and wipe the temp files

--- Glossaire/INCLUDE/attachments/medintux2tess4attachments.sh
+++ Glossaire/INCLUDE/attachments/medintux2tess4attachments.sh
@@ -6,24 +6,26 @@
 # MedinTux
 # This work is based on the original medintux2tess
 
-ERRORLOG="medintux2tess--"$(date +"%Y-%m-%d-%H-%M")".log" # file where STDERR goes
+ERRORLOG="medintux2tess4attachments--"$(date +"%Y-%m-%d-%H-%M")".log" # file where STDERR goes
+USE_WILDCARD=$1
+DO_OCR=$5
 TES_LANG=$2
 TEMP_DIR=$3
+LOG_DIR=$6
 
 #............... redirect STDERR to ERRORLOG ...............
-exec 2>>$TEMP_DIR$ERRORLOG
+exec 2>>$LOG_DIR$ERRORLOG
 
 if [ $4 = 'debug' ]; then
 	echo "=========================================">&2
-	echo " medintux2tess">&2
+	echo " medintux2tess4attachments">&2
 	echo "=========================================">&2
-	echo "------------ arguments-------------------">&2
-	echo $1>&2
-	echo $2>&2
-	echo $3>&2
-	echo "------------ fichiers--------------------">&2
-	echo "Rep. Temp : "$TEMP_DIR>&2
+	echo "------------ Arguments -------------------">&2
+	echo "Wildcard : "$USE_WILDCARD>&2
 	echo "Language : "$TES_LANG>&2
+	echo "Do OCR : "$DO_OCR>&2
+	echo "Temp Dir : "$TEMP_DIR>&2
+	echo "Log DIR : "$LOG_DIR>&2
 	echo "-----------------------------------------">&2
 fi
 
@@ -37,17 +39,26 @@ rm "$TEMP_DIR"ocr_pdf.txt
 #First we copy preview_pdf_00 to be sure to keep a preview file
 cp "$TEMP_DIR"preview_pdf_00.png "$TEMP_DIR"preview_pdf.png
 
-if [ $1 = 'YES' ]; then
-		c=000
-		for i in "$TEMP_DIR"preview_pdf_*.png
-			do
-				tesseract $i "$TEMP_DIR"ocr_pdf.multi$c $TES_LANG >&2
-				c=`printf "%03d" $((c+1))`
-				rm $i
-			done
-		cat "$TEMP_DIR"ocr_pdf.multi*.txt > "$TEMP_DIR"ocr_pdf.txt
-		rm "$TEMP_DIR"ocr_pdf.multi*.txt
+#if DO_OCR is not equal to "SI" then we only delete temp files
+if [ $DO_OCR = 'SI' ]; then
+	if [ $USE_WILDCARD = 'YES' ]; then #if YES then our PDF has multiple pages
+			c=000
+			for i in "$TEMP_DIR"preview_pdf_*.png
+				do
+					tesseract $i "$TEMP_DIR"ocr_pdf.multi$c $TES_LANG >&2
+					c=`printf "%03d" $((c+1))`
+					rm $i
+				done
+			cat "$TEMP_DIR"ocr_pdf.multi*.txt > "$TEMP_DIR"ocr_pdf.txt
+			rm "$TEMP_DIR"ocr_pdf.multi*.txt
 	else
-		tesseract "$TEMP_DIR"preview_pdf_00.png "$TEMP_DIR"ocr_pdf $TES_LANG >&2
-		rm "$TEMP_DIR"preview_pdf_00.png
+			tesseract "$TEMP_DIR"preview_pdf_00.png "$TEMP_DIR"ocr_pdf $TES_LANG >&2
+			rm "$TEMP_DIR"preview_pdf_00.png
 	fi
+else
+	if [ $USE_WILDCARD = 'YES' ]; then
+			rm "$TEMP_DIR"ocr_pdf.multi*.txt
+	else
+			rm "$TEMP_DIR"preview_pdf_00.png
+	fi
+fi
