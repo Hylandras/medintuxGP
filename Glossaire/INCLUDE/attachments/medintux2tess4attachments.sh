682bea9cf1dd0f2371c896def12668899724a5bd
Hylandras <hylandras@tutanota.com>
2015-11-07 23:19:49 +0100

    Added ability to include images as PDFs
    All changes are built to make easy adding of new file types and to be
    cross-platforms as much as possible

--- Glossaire/INCLUDE/attachments/medintux2tess4attachments.sh
+++ Glossaire/INCLUDE/attachments/medintux2tess4attachments.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 ##############################################################################
-# medintux2tess4attachments
+#medintux2tess4attachments
 ##############################################################################
-# medintux2tess4attachments is a TesseractOCR wrapper to be able to use tesseract with
+#medintux2tess4attachments is a TesseractOCR wrapper to be able to use tesseract with
 # MedinTux
 # This work is based on the original medintux2tess
 
-ERRORLOG="medintux2tess4attachments--"$(date +"%Y-%m-%d-%H-%M")".log" # file where STDERR goes
-USE_WILDCARD=$1
-DO_OCR=$5
-TES_LANG=$2
-TEMP_DIR=$3
-LOG_DIR=$6
+ERRORLOG="medintux2tess4pdfattachments--"$(date +"%Y-%m-%d-%H-%M")".log" # file where STDERR goes
+USE_WILDCARD=$1 #PDF only : if YES then our PDF has multiple pages
+TES_LANG=$2 #Language setting for Tesseract
+TEMP_DIR=$3 #Directory of temporary files
+#the fourth argument is not because we only need it once for debug
+DO_OCR=$5 #PDF only : if NO then we only delete temporary preview files
+LOG_DIR=$6 #Directory of Log Files
+FILE_NAME=$7 #File name is useful for images where extension can change, when we call the script for a PDF we give "pdf" as value so we can easily test if we need to run this script for a simple picture or a PDF file without needing a tenth variable
+LAST_NAME=$8 #It's the final name (with extension) of the file for which we run an OCR
+PAT_DIR=$9 #It's the directory where files are finally stored
 
 #............... redirect STDERR to ERRORLOG ...............
 exec 2>>$LOG_DIR$ERRORLOG
 
 if [ $4 = 'debug' ]; then
 	echo "=========================================">&2
-	echo " medintux2tess4attachments">&2
+	echo "medintux2tess4attachments">&2
 	echo "=========================================">&2
 	echo "------------ Arguments -------------------">&2
 	echo "Wildcard : "$USE_WILDCARD>&2
@@ -26,39 +30,38 @@ if [ $4 = 'debug' ]; then
 	echo "Do OCR : "$DO_OCR>&2
 	echo "Temp Dir : "$TEMP_DIR>&2
 	echo "Log DIR : "$LOG_DIR>&2
+	echo "File name : "$FILE_NAME>&2
+	echo "Final name : "$LAST_NAME>&2
+	echo "Patient Dir : "$PAT_DIR>&2
 	echo "-----------------------------------------">&2
 fi
 
-#............... cleaning for the new output ...............
-#Usually ocr_pdf.txt is destroyed during the import but in case of bug
-#we perform this action so the validating test occuring after this script
-#won't return any false positive
-rm "$TEMP_DIR"ocr_pdf.txt
+if [ $FILE_NAME = 'pdf' ]; then
+			#............... start OCR (tesseract expands output with *.txt) ...............
+			#First we copy preview_pdf_00 to be sure to keep a preview file
+			cp "$TEMP_DIR"preview_pdf_00.png "$TEMP_DIR"preview_pdf.png
 
-#............... start OCR (tesseract expands output with *.txt) ...............
-#First we copy preview_pdf_00 to be sure to keep a preview file
-cp "$TEMP_DIR"preview_pdf_00.png "$TEMP_DIR"preview_pdf.png
-
-#if DO_OCR is not equal to "SI" then we only delete temp files
-if [ $DO_OCR = 'YES' ]; then
-	if [ $USE_WILDCARD = 'YES' ]; then #if YES then our PDF has multiple pages
-			c=000
-			for i in "$TEMP_DIR"preview_pdf_*.png
-				do
-					tesseract $i "$TEMP_DIR"ocr_pdf.multi$c $TES_LANG >&2
-					c=`printf "%03d" $((c+1))`
-					rm $i
-				done
-			cat "$TEMP_DIR"ocr_pdf.multi*.txt > "$TEMP_DIR"ocr_pdf.txt
-			rm "$TEMP_DIR"ocr_pdf.multi*.txt
-	else
-			tesseract "$TEMP_DIR"preview_pdf_00.png "$TEMP_DIR"ocr_pdf $TES_LANG >&2
-			rm "$TEMP_DIR"preview_pdf_00.png
-	fi
+			if [ $DO_OCR = 'YES' ]; then
+				if [ $USE_WILDCARD = 'YES' ]; then
+						c=000
+						for i in "$TEMP_DIR"preview_pdf_*.png
+							do
+								tesseract $i "$TEMP_DIR"ocr_pdf.multi$c $TES_LANG >&2
+								c=`printf "%03d" $((c+1))`
+								rm $i
+							done
+						cat "$TEMP_DIR"ocr_pdf.multi*.txt > "$PAT_DIR""$LAST_NAME".ocr.txt
+						rm "$TEMP_DIR"ocr_pdf.multi*.txt
+				else
+						tesseract "$TEMP_DIR"preview_pdf_00.png "$PAT_DIR""$LAST_NAME".ocr $TES_LANG >&2
+						rm "$TEMP_DIR"preview_pdf_00.png
+				fi
+		else
+				for i in "$TEMP_DIR"preview_pdf_*.png
+					do
+						rm $i
+					done
+		fi
 else
-	if [ $USE_WILDCARD = 'YES' ]; then
-			rm "$TEMP_DIR"ocr_pdf.multi*.txt
-	else
-			rm "$TEMP_DIR"preview_pdf_00.png
-	fi
+		tesseract "$TEMP_DIR""$FILE_NAME" "$PAT_DIR""$LAST_NAME".ocr $TES_LANG >&2
 fi
